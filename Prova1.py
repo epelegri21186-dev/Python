5189a256ab9e0abdb67127879ab2e228bd11c038
epelegri21186-dev <epelegri21186@iesjoanramis.org>
2025-11-12 13:17:54 +0100

    primer ejercicio de python

---
+++ Prova1.py
@@ -0,0 +1,5 @@
+a = float(input("Escriure el primer nombre: "))
+b = float(input("Escriure el segon nombre: "))
+d=float(input("nombre:"))
+c = a + b + d
+print("El resultat de la suma és {}".format(c))
