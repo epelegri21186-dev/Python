be852c1311925a437439493d9175f26cb37c4137
epelegri21186-dev <epelegri21186@iesjoanramis.org>
2025-11-25 21:18:08 +0100

    Ejercici 21

--- Ejercici 21.py
+++ Ejercici 21.py
@@ -1,21 +1,13 @@
-'''Definir una funció es_palindrom() que retorni vertader si li passem 
-un palíndrom i fals en cas contrari. Un palíndrom és una paraula 
- que s’escriu igual d’esquerra a dreta i de dreta a esquerra. 
- Per exemple: radar, ara, civic, rallar, tapat, simis, refer'''
-
-def es_palindrom(para):
-    para=list(a)
-    b= para.copy()
-    para.reverse()
-    if para==b:
+def es_palindrom(parula):
+    parula=list(a)
+    b= parula.copy()
+    parula.reverse()
+    if parula==b:
         print('TRUE')
     else:
         print('FALSE')
 
-
-
-
-#Program principal
-para=0
+# Programa principal
+paraula =0
 a=input('introdureix una parula: ')
-es_palindrom(para)
+es_palindrom(paraula)
