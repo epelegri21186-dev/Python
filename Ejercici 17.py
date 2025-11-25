6624c6219e6a51d1c8a1095a87d7b806f5d76212
epelegri21186-dev <epelegri21186@iesjoanramis.org>
2025-11-25 16:25:26 +0100

    ex17

---
+++ Ejercici 17.py
@@ -0,0 +1,11 @@
+'''Definir una funció que calculi la longitud
+ d’una llista o d’una cadena donada. 
+Prova-la amb diferents exemples.'''
+lon=()
+def ex17(llista):
+    llista=list(3)
+    lon=len(llista)
+
+llista=(8)
+ex17
+print(lon)
