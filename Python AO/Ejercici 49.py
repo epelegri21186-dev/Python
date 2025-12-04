2962fda451cf1e0aca0f589016c486e4d5f73100
epelegri21186-dev <epelegri21186@iesjoanramis.org>
2025-12-04 12:03:03 +0100

    ex

--- Python AO/Ejercici 49.py
+++ Python AO/Ejercici 49.py
@@ -1,10 +1,13 @@
 def hi_ha_duplicats(llista, conjunt):
     if len(llista)==len(conjunt):
-        print('No hi ha cap repetit')
+        print(f'No hi ha cap repetit en la llista {llista}')
     else:
-        print('Hi ha caracters repetits')
+        print(f'Hi ha caracters repetits en la llista {llista}')
 
 
 llista=[1,2,3,4,5,7,8,6]
 conjunt=set(llista)
+llista2=[1,2,2,4,5,7,8]
+conjunt2=set(llista2)
 hi_ha_duplicats(llista, conjunt)
+hi_ha_duplicats(llista2, conjunt2)
